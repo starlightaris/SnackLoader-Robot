561bc806b7ce7f70293491c22bb1e14ca17aeb60
Thilini Piyumika <piyumikat80@gmail.com>
2025-11-22 16:40:04 +0530

    updated cat's loadcell file

--- src/slave/LoadCell/arduino-slave-Loadcell-cat.ino
+++ src/slave/LoadCell/arduino-slave-Loadcell-cat.ino
@@ -1,49 +1,42 @@
 #include "HX711.h"
- 
+
 #define DT 2
-
 #define SCK 3
- 
+
 HX711 scale;
- 
-float calibration_factor = 476474.27;
- 
+
+
+float calibration_factor = 471709.53;
+
 void setup() {
-
   Serial.begin(9600);
-
   scale.begin(DT, SCK);
- 
+
   scale.set_scale(calibration_factor);
- 
+
   delay(1000);
-
   Serial.println("Make sure NO weight is on the scale now...");
-
   delay(1500);
 
-  scale.tare();
- 
+  scale.tare();   
+
   Serial.print("Calibration factor set to: ");
-
   Serial.println(calibration_factor, 2);
-
   Serial.println("Place your test weight now.");
-
 }
- 
+
 void loop() {
- 
-  float weightKg = scale.get_units(20);
- 
-  if (weightKg < 0.0001) weightKg = 0;
- 
-  Serial.print(weightKg, 3);
+  
+  float weightKg = scale.get_units(50);
 
-  Serial.println(" CAT");
- 
-  delay(500);
+  if (weightKg < 0) weightKg = 0;
 
+  
+  float weightGrams = weightKg * 1000.0;
+
+  
+  Serial.print(weightGrams, 1);
+  Serial.println(" g");
+
+  delay(50);
 }
-
- 
